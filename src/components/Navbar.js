580ecddbe2ace5b0f806cb163ad9be2b1d9e2064
Micah-Slagle <mjs2017@live.missouristate.edu>
2020-03-05 02:05:00 +0000

    Registered a service worker: the react default was failing for me.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return(
         <nav id="navBar">
-            <Link to="/" class="navKey">Recent Photos</Link>
-            <Link to="/Mlb" class="navKey">MLB</Link>
-            <Link to="/Mlb" class="navKey">NFL</Link>
-            <Link to="/Mlb" class="navKey">NBA</Link>
-            <Link to="/Mlb" class="navKey">MLS</Link>
-            <Link to="/Mlb" class="navKey">NHL</Link>
+            <Link to="/" className="navKey">Recent Photos</Link>
+            <Link to="/Mlb" className="navKey">MLB</Link>
+            <Link to="/Mlb" className="navKey">NFL</Link>
+            <Link to="/Mlb" className="navKey">NBA</Link>
+            <Link to="/Mlb" className="navKey">MLS</Link>
+            <Link to="/Mlb" className="navKey">NHL</Link>
         </nav>
     )
 }
